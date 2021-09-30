4138c14906d3140e9353b1e6b3efb871ff633f26
Burak <burakkepuc.1996@gmail.com>
2021-09-30 23:51:00 +0300

    app functionality done

--- app.js
+++ app.js
@@ -1,24 +1,80 @@
  let container = document.getElementById('container');
+ let btn = document.getElementsByClassName('btn');
+
+
  let cell;
+ let gridSize = 10;
 
-function makeGrid(size){
 
-    container.style.setProperty('--grid-column',size);
-    container.style.setProperty('--grid-rows',size);
+ function randomRGB() {
+     var x = Math.floor(Math.random() * 256);
+     var y = Math.floor(Math.random() * 256);
+     var z = Math.floor(Math.random() * 256);
+     return RGBColor = "rgb(" + x + "," + y + "," + z + ")";
+ }
 
- for(let i = 0; i < size*size; i++){
+
+
+ function makeGrid(size) {
+
+     container.style.setProperty('--grid-column', size);
+     container.style.setProperty('--grid-rows', size);
+
+     for (let i = 0; i < size * size; i++) {
          cell = document.createElement('div');
-        //cell.innerText=  ;
-        container.appendChild(cell);
-        cell.classList.add('box');
-
+         container.appendChild(cell);
+         cell.classList.add('box');
+     };
  };
-};
 
-makeGrid(30);
+ gameInit();
+
+ function mouseOverColorful() {
+     container.addEventListener("mouseover", (event) => {
+         return event.target.style.background = randomRGB();
+
+     });
+ }
+
+ function mouseOverBlack() {
+     container.addEventListener("mouseover", (event) => {
+         return event.target.style.background = "black";
+
+     });
+ }
 
 
- container.addEventListener("mouseover",(event) => {
-     //event.target.classList.add('bg-color');
+
+ btn[0].addEventListener("click", () => {
+
+     container.innerHTML = '';
+
+     gridSize = prompt("Enter a grid size(Between 1 and 100)");
+     if (gridSize > 100 || gridSize < 1) {
+         alert("Invalid Size! Try Again !");
+         makeGrid(5);
+
+     } else {
+         makeGrid(gridSize);
+     }
+
  });
 
+ btn[1].addEventListener("click", () => {
+     container.innerHTML = '';
+     container.style.background = 'white';
+     makeGrid(gridSize);
+ });
+
+ btn[2].addEventListener("click", (event) => {
+     mouseOverBlack();
+ });
+
+ btn[3].addEventListener("click", (event) => {
+     mouseOverColorful();
+ });
+
+ function gameInit() {
+     mouseOverColorful();
+     makeGrid(5);
+ }
